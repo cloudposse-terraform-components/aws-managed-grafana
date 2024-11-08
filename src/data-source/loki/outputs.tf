1d4550f67f65faa19034dd9687fb0d5bf7931e08
Igor Rodionov <goruha@gmail.com>
2024-11-08 18:33:04 +0100

    Initial commit

--- src/data-source/loki/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "uid" {
-  # The output id is not the uid. It follows a format like "1:uid"
-  value       = split(":", grafana_data_source.loki[0].id)[1]
-  description = "The UID of this dashboard"
-}
